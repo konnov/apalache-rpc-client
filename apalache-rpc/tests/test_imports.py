864562a93db7b73faed7b55a704d2ddd2ba28e8b
Igor Konnov <igor@konnov.phd>
2025-11-12 12:30:31 +0100

    add a trivial test

---
+++ apalache-rpc/tests/test_imports.py
@@ -0,0 +1,9 @@
+from apalache_rpc.client import JsonRpcClient
+
+
+class TestImports:
+    """Simple test to see whether imports work."""
+
+    def test_ok(self):
+        """Trivial test"""
+        pass
